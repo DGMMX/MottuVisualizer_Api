0c3816619236ed9d06414e5575be2883f7c93d52
DGMMX <dgmmxs2@outlook.com>
2025-11-05 22:22:01 -0300

    ...

--- MottuVisualizer_Api/Properties/Dtos/UsuarioDto.cs
+++
@@ -1,9 +0,0 @@
-namespace MotoMonitoramento.DTOs
-{
-    public class UsuarioDto
-    {
-        public string Nome { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string Senha { get; set; } = string.Empty;
-    }
-}
